c43eaf2f68e8acaae6a08ec6c0bd1e83d7165959
jack <jackjoniur7@gmail.com>
2021-09-09 08:52:36 +0430

    add js file for html tags

---
+++ js/project.js
@@ -0,0 +1,13 @@
+// function for onblur image :)
+function Click() {
+  let find, oper;
+  find = document.getElementsByClassName("image-Ms")[0];
+  oper = document.getElementsByClassName("icon")[0];
+  oper.style.zIndex = 0;
+  find.style.filter = "blur(0px)";
+}
+// function for type message :)
+function message() {
+    let find;
+    find = document.getElementById()
+}
