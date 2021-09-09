f7d8f96c6b3186025527e77aa61cf2b63a715067
jack <jackjoniur7@gmail.com>
2021-09-09 11:00:54 +0430

    add chat opption for chat box

--- js/project.js
+++ js/project.js
@@ -8,6 +8,34 @@ function Click() {
 }
 // function for type message :)
 function message() {
-    let find;
-    find = document.getElementById()
+  let create, inp, timeline, span, date;
+  create = document.createElement("p");
+  inp = document.getElementsByClassName("inp-type")[0];
+  document.getElementsByClassName("chat-box")[0].appendChild(create);
+  create.innerHTML = inp.value;
+  create.classList.add("Ms-me");
+
+  timeline = document.createElement("p");
+  span = document.createElement("span");
+  document.getElementsByClassName("chat-box")[0].appendChild(timeline);
+  timeline.classList.add("caption-me");
+  timeline.innerHTML = "you ";
+  let test = document.getElementsByClassName("caption-me");
+  // am and pm fotmatter
+  function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    return strTime;
+  }
+  // end am and pm fotmatter
+  for (let i = 2; i < test.length; i++) {
+   test[i].appendChild(span);
+   span.innerHTML =  formatAMPM(new Date);
+  }
+  inp.value = null;
 }
